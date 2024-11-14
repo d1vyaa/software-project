f965a6b6af8700864aaba53000d1ae321918ef79
Divya Shubhangi <141157701+d1vyaa@users.noreply.github.com>
2024-11-15 01:26:33 +0530

    Add files via upload

---
+++ node.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, key):
+        self.key=key
+        self.left=None
+        self.right=None
+        self.height=1
+        pass
+
+#################################GurnoorSingh###################################################
+#################################GurnoorSingh###################################################
